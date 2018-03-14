ea11ee2a7059ceadd32ee5ebfbd614357d5685a6
Dan <dpedersen84@gmail.com>
2018-03-13 21:43:26 -0500

    psudeo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//start button
+
+//1 question at a time 
+    //30 second timer per question
+    //multiple choice
+        //highlights question selected
+        //incorrect or correct page / time out page with video? image?
+            //moves to next question in 3-5 seconds
+
+//game over screen
+    //correct / incorrect total
+    //new game button
+        //no need to refresh page
